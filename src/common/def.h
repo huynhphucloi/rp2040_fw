a5f8f58f12835b342000736adc7b5fa271a9d6e8
phucloi97 <phucloi97@hotmail.com>
2023-08-05 19:06:58 +0700

    include header common

---
+++ src/common/def.h
@@ -0,0 +1,18 @@
+#ifndef SRC_COMMON_DEF_H_
+#define SRC_COMMON_DEF_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef max
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+#endif
